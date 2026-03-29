2d5b9e61abb29d0417e466e6a779e4ac71642c4f
doneternity <doneternity109@gmail.com>
2026-03-29 11:09:03 +0200

    fix dashboard redirect option

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,9 @@ import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
 import { NextRequest } from "next/server";
 
 export default function middleware(req: NextRequest) {
-  return withAuth(req);
+  return withAuth(req, {
+    isReturnToCurrentPage: true,
+  });
 }
 
 export const config = {
